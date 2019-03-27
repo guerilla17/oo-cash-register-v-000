cf84ef376cd7522828bc9772f1fcd1fabab23920
Gigi M <mmgamao1@gmail.com>
2019-03-27 17:31:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class Total  
+  attr_accessor :transaction  
+  total = 0  
+   def initialize(transaction)
+     @
+  
+  def apply_discount
+    self.
+  end
+  
+  def void_last_transaction 
+    
+  end
+  
+end
 
